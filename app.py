edb6bc478515639adb39ba39700699ba79c80573
Sourav-rakshe <souravrakshe35@gmail.com>
2026-08-16 12:36:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ my name is kiren
 I am from satara
 I am 26 years old
 Currently I have nearly 3 yrs of experience in Banking & telecom domain.
+At prenset I am working As a technical support engineer at clover infotech for the yes back project 
